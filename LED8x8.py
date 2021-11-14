f09dbde731b0a1729ff7b5a407f8f59fc75f1111
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-11-14 18:48:39 +0000

    lightning bug

--- LED8x8.py
+++ LED8x8.py
@@ -1,37 +1,68 @@
 from shifter import Shifter
 import time
+import multiprocessing
+from random import randint
 
 class LED8x8():
-  pattern = [
-    0b11000011, 
-    0b10111101, 
-    0b01011010,
-    0b01111110,
-    0b01011010,
-    0b01100110, 
-    0b10111101, 
-    0b11000011]
-
   row = [
     0b10000000,
     0b01000000,
-    0b00100000,
-    0b00010000,
-    0b00001000,
-    0b00000100,
-    0b00000010,
-    0b00000001
-  ]
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000 
+    ]
 
+  pattern = [
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b11111111
+    ]
 
-  
   'LED 8x8 Class'
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
+    patternArray= multiprocessing.Array('i', 8)
+    rowArray= multiprocessing.Array('i', 8)
+    displayProcess= multiprocessing.Process(name='Display',target=LED8x8.lightningBug, args=(patternArray,rowArray))
+
+    displayProcess.daemon = True
+    displayProcess.start()
+   
 
   def display(self):
     for n in range(0,8):
-      self.shifter.shiftByte(LED8x8.pattern[n]) # load the row values
-      self.shifter.shiftByte(LED8x8.row[n]) # select the given row
+      self.shifter.shiftByte(LED8x8.patternArray[n]) # load the row values
+      self.shifter.shiftByte(LED8x8.rowArray[n]) # select the given row
       self.shifter.ping(self.shifter.latchPin)
+    time.sleep(0.001)
+
+  def lightningBug(self):
+    i=0
+    j=0
+    while True:
+      try:
+        x=randint(-1,1)  # defines motion along row                
+        y=randint(-1,1) # defines motion along column
+       
+        if (x+i)>8 or (x+i)<0:
+          x=-x
+        if (y+j)>8 or (y+j)<0:
+          y=-y
+        i= x+i #current location on row (column #)
+        j= y+j #current location on column (row #)
+        LED8x8.patternArray[j]=0b00000000
+        LED8x8.patternArray[y+j]=(0b00000000)|(1<<(x+i))
+        time.sleep(.1)
+
+
+      except Exception as e:
+        print(e)
--- Lab6.py
+++ Lab6.py
@@ -1,5 +1,5 @@
-import time
 from LED8x8 import LED8x8
+from random import randint
 
 # Simple demonstration of the LEDdisplay class.
 # Note that we don't need RPi.GPIO here since all the I/O
@@ -9,14 +9,15 @@ from LED8x8 import LED8x8
 
 dataPin, latchPin, clockPin = 23, 24, 25
 
-# Pick a number sequence
-sequence = [1, 2, 3, 4, 5, 6, 7, 8]
-
 ourLED= LED8x8(dataPin, latchPin, clockPin)
-
 while True:
   try:
     ourLED.display()
-    time.sleep(0.001)
+
   except Exception as e:
     print(e)
+    ourLED.displayProcess.terminate()
+    ourLED.displayProcess.join(2)
+
+
+
